c6dd98c3744d1d7aa75fbb8b2f45d4a7f0c55f21
Isaias Tavares <isaiasengsoft@gmail.com>
2024-02-29 00:27:14 -0300

    Update ssr-form-source.js

--- ssr-form-source.js
+++ ssr-form-source.js
@@ -1,9 +1,8 @@
-var iframe = document.getElementById('ssr-form');
-
-if (iframe) {
-    var currentSrc = iframe.src;
-    iframe.src = currentSrc + (currentSrc.includes('?') ? '&' : '?') + 'source=' + encodeURIComponent(window.location.href);
-
-    var pageTitle = document.title;
-    iframe.contentDocument.title = pageTitle;
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var iframe = document.getElementById('ssr-form');
+    
+    if (iframe) {
+        var currentSrc = iframe.src;
+        iframe.src = currentSrc + (currentSrc.includes('?') ? '&' : '?') + 'source=' + encodeURIComponent(window.location.href);
+    }
+});
