e34b1587b78da5fee253a34a5c2f059fa77deb12
Isaias Tavares <isaiasengsoft@gmail.com>
2024-04-08 09:56:54 -0300

    fix iframe load twice

--- ssr-form-source.js
+++ ssr-form-source.js
@@ -1,10 +1,13 @@
+var iframeLoaded = false;
+
 window.addEventListener('load', function () {
     var iframe = document.getElementById('ssr-form');
     
-    if (iframe) {
+    if (iframe && !iframeLoaded) {
         var currentSrc = iframe.src;
         iframe.src = currentSrc + (currentSrc.includes('?') ? '&' : '?') + 'utm_source=' + encodeURIComponent(window.location.href);
+        iframeLoaded = true;
     } else {
-        console.log('I didn\'t find the form');
+        console.log('I didn\'t find the form or it\'s already loaded');
     }
 });
