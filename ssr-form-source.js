4e83c88740c903ccd7493136cc53cd79b187d476
Isaias Tavares <isaiasengsoft@gmail.com>
2024-04-21 23:07:48 -0300

    add query params to src iframe

--- ssr-form-source.js
+++ ssr-form-source.js
@@ -1,22 +1,16 @@
-//var iframeLoaded = false;
-
 window.addEventListener('load', function () {
-    // Obter o valor atual do atributo src
+    const queryParams = window.location.search.substring(1).split('&');
+    var queryString = '';
+
+    queryString += (index === 0 ? '#' : '&') + 'source=' + document.referrer
+    
+    queryParams.forEach(function(param, index) {
+        queryString += (index === 0 ? '#' : '&') + param;
+    });
+    
     var currentSrc = $('#ssr-form').attr('src');
     
-    // Adicionar o parâmetro utm_source
-    var newSrc = currentSrc + (currentSrc.includes('?') ? '#' : '#') + 'utm_source=' + encodeURIComponent(window.location.href);
+    var newSrc = currentSrc + queryString;
     
-    // Definir o novo valor do atributo src
     $('#ssr-form').attr('src', newSrc);
-    /*
-    var iframe = document.getElementById('ssr-form');
-    
-    if (iframe && !iframeLoaded) {
-        var currentSrc = iframe.src;
-        iframe.src = currentSrc + (currentSrc.includes('?') ? '&' : '?') + 'utm_source=' + encodeURIComponent(window.location.href);
-        iframeLoaded = true;
-    } else {
-        console.log('I didn\'t find the form or it\'s already loaded');
-    }*/
 });
